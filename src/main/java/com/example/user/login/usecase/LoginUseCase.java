d211c0ce6c7eca6000e376809f7193e929438cb3
choiyongbum <72883647+choiyongbum@users.noreply.github.com>
2025-02-17 20:15:53 +0900

    로그인 컨트롤러 옵셔널 코드 수정

--- src/main/java/com/example/user/login/usecase/LoginUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.example.user.login.usecase;
-
-import com.example.user.member.domain.Member;
-import java.util.Optional;
-
-public interface LoginUseCase {
-    Optional<Member> login(String loginId, String password);
-}
