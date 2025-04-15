1f5b5299c0fb1b0347cb1e8c4b0692a84623a592
choiyongbum <72883647+choiyongbum@users.noreply.github.com>
2025-04-15 22:05:23 +0900

    Design : 디자인 변경

--- src/main/java/com/example/product/repository/ItemRepository.java
+++ src/main/java/com/example/product/repository/ItemRepository.java
@@ -4,6 +4,7 @@ import com.example.product.domain.Item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.security.core.parameters.P;
 
 import java.util.List;
@@ -13,6 +14,7 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     Optional<Item> findProductById(Long id);
     Optional<Item> findProductByUserId(String userId);
 
+    @Query("SELECT i FROM Item i WHERE LOWER(i.itemName) LIKE LOWER(CONCAT('%', :searchKeyword, '%'))")
     Page<Item> findByItemNameContaining(String searchKeyword, Pageable pageable);
 
     Page<Item> findByUserId(String userId, Pageable pageable);
